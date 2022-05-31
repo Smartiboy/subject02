801aab142c041c5d65380871f0675e7c50714fa4
Jimi <viidream@163.com>
2022-05-31 11:04:50 +0800

    init

---
+++ src/components/DataList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">List</div>
+</template>
+
+<script>
+export default {
+  name: 'DataList',
+  components: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  mounted() {
+  },
+  methods: {}
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
